a06c2cd7e23c28f414718644b04c994fe4bc62d0
NelliEfr <n.efremyan@gmail.com>
2024-12-23 19:32:53 +0400

    v1.2

--- src/entities/cart/model/types.ts
+++
@@ -1,5 +0,0 @@
-import { type Product } from "@/entities/product/model/types";
-
-export interface CartProduct extends Product {
-    count: number; 
-}
--- src/widgets/shopping-cart-list/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as ShoppingCartList } from './ui.vue'
